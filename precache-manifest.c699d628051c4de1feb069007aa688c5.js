f444261798067b973a94b8d8940053a850bd14c4
oschuang <oschuang1@gmail.com>
2020-04-24 13:57:58 -0400

    Updates

---
+++ precache-manifest.c699d628051c4de1feb069007aa688c5.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f0ab3f6a72245541fc1a9d6c76db8c08",
+    "url": "/circles-memory-game/index.html"
+  },
+  {
+    "revision": "b25a4df05440d5256317",
+    "url": "/circles-memory-game/static/css/main.71b9820d.chunk.css"
+  },
+  {
+    "revision": "ba0687b0fa9bf94626a9",
+    "url": "/circles-memory-game/static/js/2.a4b3c4ad.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/circles-memory-game/static/js/2.a4b3c4ad.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "b25a4df05440d5256317",
+    "url": "/circles-memory-game/static/js/main.3f198a4d.chunk.js"
+  },
+  {
+    "revision": "4bb38830fe797f1b4a35",
+    "url": "/circles-memory-game/static/js/runtime-main.6ee444a8.js"
+  }
+]);
